52055ff473d139ccf46369d1e07323f9fdafb7ac
Lincoln Marcolongo <mlincoln1205@gmail.com>
2020-11-20 09:31:18 +0100

    1 day Mr Cocktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,33 @@
+class DosesController < ApplicationController
+  def index
+    @doses = Dose.all
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to @dose.cocktail
+    else
+      redirect_to new_cocktail_dose_path
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail), notice: 'Dose was succesfully destroyed'
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ingredient = Ingredient.create(
+[
+{
+name: "Light rum"
+},
+{
+name: "Applejack"
+},
+{
+name: "Gin"
+},
+{
+name: "Dark rum"
+},
+{
+name: "Sweet Vermouth"
+},
+{
+name: "Strawberry schnapps"
+},
+{
+name: "Scotch"
+},
+{
+name: "Apricot brandy"
+},
+{
+name: "Triple sec"
+},
+{
+name: "Southern Comfort"
+},
+{
+name: "Orange bitters"
+},
+{
+name: "Brandy"
+},
+{
+name: "Lemon vodka"
+},
+{
+name: "Blended whiskey"
+},
+{
+name: "Dry Vermouth"
+},
+{
+name: "Amaretto"
+}]
+)
