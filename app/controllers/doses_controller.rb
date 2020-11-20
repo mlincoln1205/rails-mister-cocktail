6a4228de90078a28e3dd2fa55da3e6f539a60a45
Lincoln Marcolongo <mlincoln1205@gmail.com>
2020-11-20 20:04:38 +0100

    Mister Cocktail with image link

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -28,6 +28,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :ingredient_id)
+    params.require(:dose).permit(:description, :ingredient_id, :photo)
   end
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,4 +3,5 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
   validates :description, :cocktail_id, :ingredient_id, presence: true
   validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+  has_one_attached :photo
 end
